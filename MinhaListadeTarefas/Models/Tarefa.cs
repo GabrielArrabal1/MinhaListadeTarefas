212fd40f383c34cea6ed5fa9938940d2da237a48
Gabriel Arrabal <gabrielarrabal1@hotmail.com>
2025-04-09 17:49:30 -0300

    Primeiro commit

--- MinhaListadeTarefas/Models/Tarefa.cs
+++ MinhaListadeTarefas/Models/Tarefa.cs
@@ -5,7 +5,7 @@ namespace MinhaListadeTarefas.Models
 {
     public class Tarefa
     {
-        [Required(ErrorMessage ="O campo Id é obrigatorio")]
+        
         [Display(Name = "ID da tarefa")]
         [Range(0, int.MaxValue, ErrorMessage = "Insira um valor valido")]
         [Key]
@@ -13,42 +13,42 @@ namespace MinhaListadeTarefas.Models
         public int? Id { get; set; }
 
         [Required(ErrorMessage = "O campo Descrição é obrigatorio")]
-        [Display(Name = "Descrição da tarefa")]
+        [Display(Name = "Descrição do Evento")]
         [MaxLength(255)]
         public string? Descricao { get; set; }
         [Required(ErrorMessage = "O campo Data de Ínicio é obrigatorio")]
-        [Display(Name = "Data de Ínicio da tarefa")]
+        [Display(Name = "Data de Ínicio do Evento")]
         [DataType(DataType.Date)]
         public DateTime? Datainicio { get; set; }
         [Required(ErrorMessage = "O campo Data de Fim é obrigatorio")]
-        [Display(Name = "Data de Fim da tarefa")]
+        [Display(Name = "Data de Fim do Evento")]
         [DataType(DataType.Date)]
         public DateTime? Datafim { get; set; }
 
         [ForeignKey("Prioridade")]
         [Display(Name = "Prioridade")]
         public int PrioridadeId { get; set; }
-        public virtual Prioridade Prioridade { get; set; }
+        public virtual Prioridade? Prioridade { get; set; }
 
         [ForeignKey("Categoria")]
         [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
-        public virtual Categoria Categoria { get; set; }
+        public virtual Categoria? Categoria { get; set; }
 
         [ForeignKey("Status")]
         [Display(Name = "Status")]
         public int StatusId { get; set; }
-        public virtual Status Status { get; set; }
+        public virtual Status? Status { get; set; }
 
         public DateTime PrazoConclusao { get; set; }
-
-        [MaxLength(5000)]
-        public int Observacao { get; set; }
+        
+        public string? Observacao { get; set; }
 
         [ForeignKey("Responsavel")]
         [Display(Name = "Responsavel")]
         public int ResponsavelId { get; set; }
 
-        public virtual Responsavel Responsavel { get; set; }
+        public virtual Responsavel? Responsavel { get; set; }
+      
     }
 }
